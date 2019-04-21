863958fefe553e70716bcbaea41d8051ab42db01
Bui Kiem <buivantrung1105@gmail.com>
2019-04-22 00:16:56 +0700

    Migrate the database | Fix module errors (p.15)

--- django/config/settings.py
+++ django/config/settings.py
@@ -50,7 +50,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'django.urls'
+ROOT_URLCONF = 'config.urls'
 
 TEMPLATES = [
     {
@@ -68,7 +68,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'django.wsgi.application'
+WSGI_APPLICATION = 'config.wsgi.application'
 
 
 # Database
